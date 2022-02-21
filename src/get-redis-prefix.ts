5f298068ebb9042da3897a26d36e8a23d0a55f4a
Thiago Santos <tos.oliveira@gmail.com>
2022-02-20 22:35:59 -0300

    fix: making lint correct itself

--- src/get-redis-prefix.ts
+++ src/get-redis-prefix.ts
@@ -2,9 +2,7 @@ export function getRedisPrefix(redisPrefix: string | undefined) {
 	let result: string;
 	if (redisPrefix) {
 		result = redisPrefix;
-		if (!redisPrefix.endsWith(':')) {
-			result += ':';
-		}
+		if (!redisPrefix.endsWith(':')) { result += ':'; }
 	} else {
 		result = '';
 	}
